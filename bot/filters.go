47e7930e1a5d3dc0d6a0d9ffb2e03a9971cd5c51
amirhosss <amirhosseinleo@gmail.com>
2023-06-09 17:10:49 +0330

    A function for checking messages created

--- bot/filters.go
+++ bot/filters.go
@@ -3,29 +3,49 @@ package bot
 import (
 	"log"
 
-	conf "kheft/bot/configs"
-
 	"github.com/PaulSonOfLars/gotgbot/v2"
 	"github.com/PaulSonOfLars/gotgbot/v2/ext/handlers/filters"
 )
 
-func CheckMembership(b *gotgbot.Bot, state bool) filters.Message {
-	// state parameter reverse the return value
+type CheckMembershipOpts struct {
+	// state parameter should reverse the return value
+	ReverseState bool
+	MessageText  string
+}
+
+func (opts *CheckMembershipOpts) checkText(m *gotgbot.Message) bool {
+	if opts.MessageText == "" || opts.MessageText == m.Text {
+		return true
+	}
+	return false
+}
+
+// This function could check the membership of specific channel and equality of text message
+func (opts *CheckMembershipOpts) CheckMessage(b *gotgbot.Bot) filters.Message {
 
 	return func(m *gotgbot.Message) bool {
-		chatMember, err := b.GetChatMember(conf.Configs.ChannelChatId, m.Chat.Id, nil)
+		chatMember, err := b.GetChatMember(Configs.ChannelChatId, m.Chat.Id, nil)
 		if err != nil {
 			log.Printf("cannot get status: %s", err)
 		}
-		status := chatMember.GetStatus()
-		if (status == "creator" || status == "member") && state {
-			return true
-		} else if (status == "creator" || status == "member") && !state {
-			return false
-		} else if !state {
-			return true
-		}
 
+		switch status := chatMember.GetStatus(); status {
+		case "creator", "member":
+			if opts != nil {
+				if !opts.ReverseState && opts.checkText(m) {
+					return true
+				} else {
+					return false
+				}
+			}
+			return true
+		default:
+			if opts != nil {
+				if opts.ReverseState && opts.checkText(m) {
+					return true
+				}
+			}
+		}
 		return false
 	}
 }
