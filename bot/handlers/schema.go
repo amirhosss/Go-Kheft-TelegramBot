3a7c683d487de7d40b9fb4f1cd1dabaf18d8c9b4
amirhosss <amirhosseinleo@gmail.com>
2023-06-22 21:09:36 +0330

    Update schema and add `book` struct

--- bot/handlers/schema.go
+++ bot/handlers/schema.go
@@ -1,10 +1,15 @@
 package handlers
 
 type user struct {
-	chatId               int64
-	username             string
-	advertisePrice       int64
-	advertiseDescription string
+	chatId   int64
+	username string
+	books    []book
+}
+
+type book struct {
+	writer      string
+	description string
+	price       int64
 }
 
 var users map[int64]*user = make(map[int64]*user)
